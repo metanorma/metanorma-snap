cc4c2a8ab3f27b2995ce1bbccf72cd4f1e93e0e5
Alexande B <abobrikovich@gmail.com>
2021-03-05 15:03:30 +0300

    Improve release scripts:  - snap-set-version.sh now support different version for snap and    packed-mn download url  - allow manual releases from GitHub CI

--- snap-set-version.sh
+++ snap-set-version.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-MN_CLI_GEM_VERSION=${1:-1.2.11}
+NEW_VERSION=${1:-1.2.11}
+OLD_VERSION=$(yq e '.version' snap/snapcraft.yaml)
+PACKED_MN_ROOT_URL=https://github.com/metanorma/packed-mn/releases/download
+PACKED_MN_FILE=metanorma-linux-x64.tgz
 
-case "$OSTYPE" in
-	linux-gnu) SED=sed ;;
-	darwin*) SED=gsed ;;
-esac
+if curl --output /dev/null --silent --head --fail "${PACKED_MN_ROOT_URL}/v${NEW_VERSION}/${PACKED_MN_FILE}"; then
+  NEW_PACKED_MN_URL="${PACKED_MN_ROOT_URL}/v\$SNAPCRAFT_PROJECT_VERSION/${PACKED_MN_FILE}"
+else # URL not exists
+  NEW_PACKED_MN_URL=$(yq e '.parts.packed-mn.source' snap/snapcraft.yaml)
 
-${SED} -i "s/^version: '.*$/version: '${MN_CLI_GEM_VERSION}'/" snap/snapcraft.yaml
+  if [[ $NEW_PACKED_MN_URL == *"SNAPCRAFT_PROJECT_VERSION"* ]]; then
+	NEW_PACKED_MN_URL="${PACKED_MN_ROOT_URL}/v${OLD_VERSION}/${PACKED_MN_FILE}"
+  fi
+fi
+
+yq e ".version = \"$NEW_VERSION\"" --inplace snap/snapcraft.yaml
+yq e ".parts.packed-mn.source = \"${NEW_PACKED_MN_URL}\"" --inplace snap/snapcraft.yaml
